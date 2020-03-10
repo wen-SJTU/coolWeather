a69405f4ae3e0378d9e4082e22744193040d434e
wenchen <wenchen12@sjtu.edu.com>
2020-03-10 11:49:04 +0800

    完成遍历省市县三级列表功能

---
+++ app/src/main/java/com/example/coolweather/util/HttpUtil.java
@@ -0,0 +1,16 @@
+package com.example.coolweather.util;
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class HttpUtil {
+    public  static void sendOkHttpRequest(String address, okhttp3.Callback callback){
+        OkHttpClient client =new OkHttpClient();
+        Request request=new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+
+    }
+}
